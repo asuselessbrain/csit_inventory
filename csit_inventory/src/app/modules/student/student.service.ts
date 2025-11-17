a77ce1e6391bc41668036dc0439a8e4d60017e54
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-11-17 23:05:45 +0600

    Add: adress filtering

--- csit_inventory/src/app/modules/student/student.service.ts
+++ csit_inventory/src/app/modules/student/student.service.ts
@@ -1,19 +1,20 @@
 import { Prisma } from "../../../../generated/prisma";
 import { prisma } from "../../../shared/prisma";
 
-const getAllStudentFromDB = async(query) => {
+const getAllStudentFromDB = async (query) => {
+    const searchFields = ['name', 'email', 'address'];
     let inputFilter: Prisma.StudentWhereInput[] = []
 
-    if(query.searchTerm){
-        inputFilter.push({OR: [
-            { name: { contains: String(query.searchTerm), mode: 'insensitive' } },
-            { email: { contains: String(query.searchTerm), mode: 'insensitive' } }
-        ]})
+    if (query.searchTerm) {
+        inputFilter.push({
+            OR: searchFields.map(field => ({ [field]: { contains: String(query.searchTerm), mode: 'insensitive' } })
+            )
+        })
     }
 
-    const whereCondition: Prisma.StudentWhereInput = {AND: inputFilter}
+    const whereCondition: Prisma.StudentWhereInput = { AND: inputFilter }
 
-    const students = await prisma.student.findMany({where: whereCondition});
+    const students = await prisma.student.findMany({ where: whereCondition });
     return students;
 }
 
