5de009e577809e02d8e9d22979e9d8cac427d320
asuselessbrain <arfan18@cse.pstu.ac.bd>
2026-02-01 07:14:06 +0600

    fix: unreadable prisma validation error message

--- csit_inventory_server/src/app/errors/globalErrorHandler.ts
+++ csit_inventory_server/src/app/errors/globalErrorHandler.ts
@@ -1,22 +1,70 @@
 import { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../../generated/prisma/client";
 
 class CustomError extends Error {
-    statusCode: number;
+  statusCode: number;
 
-    constructor(message: string, statusCode: number) {
-        super(message);
-        this.statusCode = statusCode;
-        this.name = this.constructor.name; // Set the error name to the class name
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name;
+  }
+}
+
+export const globalErrorHandler = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let statusCode = 500;
+  let message = "Something went wrong!";
+  let errorMessage = error.message;
+
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === "P2002") {
+      statusCode = 409;
+      message = "Duplicate Entry";
+
+      let fieldName = "Unknown field";
+
+      if (error.meta?.target) {
+        if (Array.isArray(error.meta.target)) {
+          fieldName = error.meta.target[0];
+        } else {
+          fieldName = String(error.meta.target);
+        }
+      } else {
+        const match = error.message.match(/fields:\s*\(`?([^`)]+)`?\)/);
+        if (match && match[1]) {
+          fieldName = match[1];
+        }
+      }
+
+      errorMessage = `${fieldName} already exists. Please use a different one.`;
+    } else if (error.code === "P2025") {
+      statusCode = 404;
+      message = "Record not found";
+      errorMessage =
+        "The record you are trying to update or delete does not exist.";
     }
-}
-
-export const globalErrorHandler = (error: CustomError, req: Request, res: Response, next: NextFunction) => {
-    console.log(error)
-    res.status(error.statusCode || 500).json({
-        success: false,
-        statusCode: error.statusCode || 500,
-        message: 'An error occurred',
-        errorMessage: error.message,
-        error: error,
-    })
-}
+  }
+  else if (error instanceof Prisma.PrismaClientValidationError) {
+    statusCode = 400;
+    message = "Validation Error";
+    errorMessage = "Invalid data format or missing required fields.";
+  }
+  else if (error instanceof CustomError) {
+    statusCode = error.statusCode;
+    message = error.message;
+    errorMessage = error.message;
+  }
+    console.log(error);
+  res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message,
+    errorMessage: errorMessage,
+    error: error,
+  });
+};
